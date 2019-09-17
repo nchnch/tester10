ff8c504f1ad25e9114d38eee5d2cf4a0905ed79b
Владислав Митяшин <t89035069013@gmail.com>
2019-09-17 12:41:32 -0400

    пиксели настроил

--- tester6/Form1.Designer.cs
+++ tester6/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button8 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
@@ -52,7 +53,6 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
-            this.button8 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
@@ -95,6 +95,16 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "визуализация";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1907, 94);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(186, 62);
+            this.button8.TabIndex = 18;
+            this.button8.Text = "тест";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button7
             // 
@@ -272,7 +282,7 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(11, 237);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(3899, 1529);
+            this.pictureBox1.Size = new System.Drawing.Size(4075, 1665);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
@@ -283,16 +293,6 @@
             this.toolTip1.AutoPopDelay = 50000;
             this.toolTip1.InitialDelay = 500;
             this.toolTip1.ReshowDelay = 50;
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(1907, 94);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(186, 62);
-            this.button8.TabIndex = 18;
-            this.button8.Text = "тест";
-            this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // Form1
             // 
